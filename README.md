003b9fa4c6793416a5a6f043563ed1557412e4ce
nfaltir <nfaltir@protonmail.com>
2023-07-29 15:19:30 -0700

    v1

---
+++ README.md
@@ -0,0 +1,66 @@
+# <center>Explore Data 🔬</center>
+<img src="https://tinyurl.com/27fclmzb" style="max-width: 100%;">
+
+## Intro & App Functionalities
+
+<br>
+
+I've developed a Streamlit Python app that performs various data exploration operations on an uploaded dataset. The app offers the following functionalities:
+
+<br>
+
+1. Dataset Preview: It provides a preview of the first few rows (Head) and last few rows (Tail) of the dataset.
+2. Header Count: The app displays the total number of headers (fields or columns) present in the dataset.
+3. Row Count: It shows the total number of rows in the dataset.
+4. Total Data Points: The app calculates and presents the total number of data points in the dataset, considering all rows and columns.
+5. Dataframe Dimension Count: It provides the dimensions (rows and columns) of the dataset.
+6. Dataset Size: The app indicates the size of the dataset in terms of memory usage.
+7. List of Headers: It displays a list of all headers (fields or columns) available in the dataset.
+8. Data Types: For each header, the app presents the corresponding data type (e.g., integer, string, float, etc.).
+9. Data Summary Table: The app generates a summary table for headers with numerical values, providing statistics such as mean, median, minimum, maximum, etc.
+10. Missing Values Count: It calculates and presents the total number of missing values in the dataset.
+11. Duplicate Rows: The app identifies and provides a list of duplicate rows in the dataset.
+With these functionalities, users can efficiently explore and gain insights into their datasets.
+
+
+<br>
+
+## Why a quick data exploration is important
+
+<br>
+
+As a data practitioner encompassing roles such as Data Scientist, Data Engineer, or Data Analyst, meticulous data exploration assumes paramount significance in formulating effective strategies and refining pertinent inquiries. This allows you to ask better questions about the data and propose solutions on how to tackle the objectives set by the stakeholders, or raise concerns about the data (too many missing values, data integrity, data errors, sample size, fat-tails or etc...).
+
+
+<br>
+
+## Leverage App
+
+<br>
+
+On average quick data exploration like this will take a couple of mintues or even hours. This app generates a quick summary of the information you need before diving deeper into the data 🙃 
+
+if you cloned the repo you can use test file in project root `books.csv`
+
+Happy Coding!
+
+
+<br>
+
+## Setup
+
+- `streamlit run app.py`
+
+
+
+<img src="app.png" alt="Data Explorer"/>
+
+
+<br>
+
+## Demo Url
+
+[Data Explorer]()
+
+
+
---
+++ app.py
@@ -0,0 +1,94 @@
+import streamlit as st
+import pandas as pd
+from datetime import date
+import altair as alt
+today = date.today()
+global df 
+
+
+#streamlit config
+st.set_page_config(page_title="Explore Data", page_icon="🔬")
+
+# ---- HIDE STREAMLIT STYLE ----
+hide_st_style = """<style>footer {visibility: hidden;}</style>"""
+st.markdown(hide_st_style, unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: #D5D46F;'> Explore Data 🔬</h1>", unsafe_allow_html=True)
+st.write("<hr><br>", unsafe_allow_html=True)
+
+#setup file upload
+file_upload = st.file_uploader(label="Upload data. (400MB Max)", type=['csv', 'xlsx',])
+
+#df not defined fix
+
+if file_upload is not None:
+   
+    print(f"\n{file_upload}")
+    print("\n------------File uploaded!------------")
+    try:
+        df = pd.read_csv(file_upload)
+    except Exception as e:
+        print(e)
+        df = pd.read_excel(file_upload)
+try:
+    st.markdown("<br><br>",unsafe_allow_html=True)
+
+    
+
+    st.markdown("<h2 style='text-align:center;'>Data Preview</h2>", unsafe_allow_html=True)
+
+    #st.caption("Head")
+    st.dataframe(df.head(10))
+    st.caption("Tail")
+    st.dataframe(df.tail(10))
+    #Dataset MetaData
+    st.write("<hr><br>", unsafe_allow_html=True)
+    st.subheader("Dataset MetaData")
+    dataset_meta_data = {'Headers':[len(df.columns)],'Rows':[f'{df.shape[0]:,}'],'Total Data Points':[f'{sum(df.count()):,}'], 'DF Dimensions':[f'{df.ndim:,}'],\
+         'Dataset Size (MB)':[f'{file_upload.size/1000000:,}']}
+    df_meta_data = pd.DataFrame(data=dataset_meta_data)
+    st.table(df_meta_data)
+    
+    
+    #Header display and count
+    st.write("<hr><br>", unsafe_allow_html=True)
+    st.subheader(f'\nHeaders: {len(df.columns)}\n')
+    st.table(df.count())
+    
+    
+    #dypes
+    st.write("<hr><br>", unsafe_allow_html=True)
+    st.subheader("Header Data Types")
+    st.table(df.dtypes.astype(str))
+
+
+    #data summary
+    st.write("<hr><br>", unsafe_allow_html=True)
+    st.subheader(f'\nDataset Summary\n')
+    st.dataframe(df.describe())
+
+    
+    #missing values
+    st.write("<hr><br>", unsafe_allow_html=True)
+    st.subheader(f'\nMissing Values\n')
+    st.table(df.isna().sum())
+
+     #duplicate rows
+    st.write("<hr><br>", unsafe_allow_html=True)
+
+    st.subheader(f"Duplicate Rows")
+    st.write('click expand icon to see all data headers')
+    st.dataframe(df[df.duplicated()])
+    st.write(len(df.duplicated))
+ 
+    
+    #visualize    
+    #control_field = st.multiselect("Select A Field for a summary, values must be numerical", df.columns)
+    #st.table(df[control_field].describe())
+    #st.bar_chart(df[target_field].value_counts())
+    
+
+except Exception as e:
+    print(e)
+  
+  
+
